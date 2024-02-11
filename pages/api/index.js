c8c8e203295fbaea0dbeed53fae56464bd899be0
raghava139 <rraghava139@gmail.com>
2024-02-12 00:04:08 +0530

    PaymentAPI

---
+++ pages/api/index.js
@@ -0,0 +1,35 @@
+// pages/charge.js
+import request from 'request';
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method Not Allowed' });
+  }
+
+  const data = {
+    amount: req.body.amount,
+    source: req.body.source,
+    currency: 'usd',
+  };
+
+  // Post a charge from merchant server to clover server with api auth token
+  request.post(
+    'https://scl-sandbox.dev.clover.com/v1/charges',
+    {
+      json: data,
+      headers: {
+        accept: 'application/json',
+        authorization: 'Bearer 556fb282-1c3c-8afc-232c-8da96860b816',
+        'content-type': 'application/json',
+        'x-forwarded-for': '43XXB9T95PBSR',
+      },
+    },
+    (error, response, body) => {
+      if (error) {
+        res.status(500).json({ error: 'Failed to process the charge' });
+        return;
+      }
+      res.json(body);
+    }
+  );
+}
